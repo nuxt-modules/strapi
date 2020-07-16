4442467b294ee7352dccf3131682e20b0f89f706
Sébastien Chopin <seb@nuxtjs.com>
2020-07-16 10:36:17 +0200

    feat: use runtimeConfig to avoid building when changing Strapi URL

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    Works!
+    <h1>@nuxtjs/strapi</h1>
+    <h2>$strapi.state</h2>
+    <pre>{{ $strapi.state }}</pre>
+    <h2>options.url</h2>
+    <pre>{{ $strapi.$http._defaults.prefixUrl }}</pre>
   </div>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+div {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
-</script>
+</style>

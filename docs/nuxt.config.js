52fe078db0a14e806b8e3b2c7f3f271c6db417b9
Florent <flo@nuxtjs.com>
2020-08-03 16:23:47 +0200

    chore: add settings.json in content folder

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,5 +1,12 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-
+  env: {
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN
+  },
+  loading: { color: '#00CD81' },
+  generate: {
+    fallback: '404.html', // for Netlify
+    routes: ['/'] // give the first url to start crawling
+  }
 })

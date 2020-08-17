f328d0fac80df1e708b11f3df16b190f714ef094
Sébastien Chopin <seb@nuxtjs.com>
2020-08-17 10:40:13 +0200

    docs: add back Ackee

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,8 +1,17 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+  buildModules: [
+    // https://github.com/bdrtsky/nuxt-ackee
+    'nuxt-ackee'
+  ],
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN
   },
-  loading: { color: '#8E75FF' }
+  loading: { color: '#8E75FF' },
+  ackee: {
+    server: 'https://ackee.nuxtjs.com',
+    domainId: '4370defe-04f9-4987-82d7-817b4de6b253',
+    detailed: true
+  }
 })

5436a58039d7af2597eeec08c51e07405eed6b67
Sébastien Chopin <seb@nuxtjs.com>
2020-08-20 15:54:27 +0200

    Revert "docs: change to nuxt analytics"
    This reverts commit f7e41648724147a623e9863028deca2f2c086c83.

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,22 +1,17 @@
 import theme from '@nuxt/content-theme-docs'
 
-const script = []
-if (process.env.NODE_ENV === 'production') {
-  script.push({
-    hid: 'umami',
-    src: 'https://analytics.nuxtjs.app/umami.js',
-    'data-website-id': '597c348a-381d-4856-872e-02af59be8f2e',
-    defer: true,
-    async: true
-  })
-}
-
 export default theme({
-  head: {
-    script
-  },
+  buildModules: [
+    // https://github.com/bdrtsky/nuxt-ackee
+    'nuxt-ackee'
+  ],
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN
   },
-  loading: { color: '#8E75FF' }
+  loading: { color: '#8E75FF' },
+  ackee: {
+    server: 'https://ackee.nuxtjs.com',
+    domainId: '4370defe-04f9-4987-82d7-817b4de6b253',
+    detailed: true
+  }
 })

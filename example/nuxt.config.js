b108be7d41b69df136f56513283d92d7a9443429
Blemming <jveuxpotulsache@gmail.com>
2020-11-12 14:14:21 -0500

    feat(lib): added support for the nuxt proxy module

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -5,6 +5,19 @@ module.exports = {
   buildDir: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
   modules: [
+    '@nuxtjs/proxy',
     { handler: require('../') }
-  ]
+  ],
+  proxy: {
+    '/api': {
+      target: 'https://api.poe.house',
+      pathRewrite: {
+        '^/api': '/'
+      }
+    }
+  },
+  strapi: {
+    proxy: true,
+    prefix: '/api'
+  }
 }

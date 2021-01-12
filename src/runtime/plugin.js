7ed431da443197c6a220be178e7d2e8068345094
Pooya Parsa <pyapar@gmail.com>
2021-01-12 11:40:08 +0100

    fix(plugin): extend class proto once with options

--- src/runtime/plugin.js
+++ src/runtime/plugin.js
@@ -3,60 +3,60 @@ import { Strapi } from '@nuxtjs/strapi/runtime'
 
 const options = <%= JSON.stringify(options) %>
 
-export default async function (ctx, inject) {
-  options.entities.forEach((entity) => {
-    let key
-    let type = 'collection'
-    if (typeof entity === 'object') {
-      key = `$${entity.name}`
-      type = entity.type || 'collection'
-      entity = entity.name
-    } else {
-      key = `$${entity}`
-    }
-    if (Strapi.prototype.hasOwnProperty(key)) {
-      return
-    }
-    Object.defineProperty(Strapi.prototype, key, {
-      get () {
-        const that = this
-        return ({
-          single: {
-            find (...args) {
-              return that.find(entity, ...args)
-            },
-            update (...args) {
-              return that.update(entity, ...args)
-            },
-            delete (...args) {
-              return that.delete(entity, ...args)
-            }
+options.entities.forEach((entity) => {
+  let key
+  let type = 'collection'
+  if (typeof entity === 'object') {
+    key = `$${entity.name}`
+    type = entity.type || 'collection'
+    entity = entity.name
+  } else {
+    key = `$${entity}`
+  }
+  if (Strapi.prototype.hasOwnProperty(key)) {
+    return
+  }
+  Object.defineProperty(Strapi.prototype, key, {
+    get () {
+      const that = this
+      return ({
+        single: {
+          find (...args) {
+            return that.find(entity, ...args)
           },
-          collection: {
-            find (...args) {
-              return that.find(entity, ...args)
-            },
-            findOne (...args) {
-              return that.findOne(entity, ...args)
-            },
-            count (...args) {
-              return that.count(entity, ...args)
-            },
-            create (...args) {
-              return that.create(entity, ...args)
-            },
-            update (...args) {
-              return that.update(entity, ...args)
-            },
-            delete (...args) {
-              return that.delete(entity, ...args)
-            }
+          update (...args) {
+            return that.update(entity, ...args)
+          },
+          delete (...args) {
+            return that.delete(entity, ...args)
           }
-        })[type]
-      }
-    })
+        },
+        collection: {
+          find (...args) {
+            return that.find(entity, ...args)
+          },
+          findOne (...args) {
+            return that.findOne(entity, ...args)
+          },
+          count (...args) {
+            return that.count(entity, ...args)
+          },
+          create (...args) {
+            return that.create(entity, ...args)
+          },
+          update (...args) {
+            return that.update(entity, ...args)
+          },
+          delete (...args) {
+            return that.delete(entity, ...args)
+          }
+        }
+      })[type]
+    }
   })
+})
 
+export default async function (ctx, inject) {
   const strapi = new Strapi(ctx, options)
 
   if (process.server && !process.static) {

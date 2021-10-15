1712fe613001fb689e9d4692329ddc620926fafa
Benjamin Canac <canacb1@gmail.com>
2021-10-15 17:09:49 +0200

    chore(example): fix module load

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -3,6 +3,6 @@ import type { NuxtConfig } from '@nuxt/types'
 export default <NuxtConfig>{
   buildModules: ['@nuxt/typescript-build'],
   modules: [
-    '../src/module.ts'
+    '../src/index.ts'
   ]
 }

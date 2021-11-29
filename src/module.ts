786a9e86326d276a1b8c920b7eeae78f8dd02581
Vincent Caggiari <vincent@edencia.com>
2021-11-29 15:53:26 +0100

    fix: publicRuntimeConfig correct handling for default configuration
    Signed-off-by: Vincent Caggiari <vincent@edencia.com>

--- src/module.ts
+++ src/module.ts
@@ -23,7 +23,7 @@ async function strapiModule (moduleOptions) {
 
   nuxt.options.publicRuntimeConfig = nuxt.options.publicRuntimeConfig || {}
   nuxt.options.publicRuntimeConfig.strapi = nuxt.options.publicRuntimeConfig.strapi || {}
-  nuxt.options.publicRuntimeConfig.strapi.url = options.url
+  nuxt.options.publicRuntimeConfig.strapi.url = nuxt.options.publicRuntimeConfig.strapi.url || options.url
 
   const runtimeDir = resolve(__dirname, 'runtime')
   nuxt.options.alias['~strapi'] = runtimeDir

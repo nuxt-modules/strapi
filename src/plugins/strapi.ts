d5edd08c7ff26bf34bb80cdcab5fd3d89afdaeb9
Benjamin Canac <canacb1@gmail.com>
2021-12-07 12:00:48 +0100

    fix(plugins): missing imports

--- src/plugins/strapi.ts
+++ src/plugins/strapi.ts
@@ -1,3 +1,6 @@
+import { useStrapiVersion } from '../composables/useStrapiVersion'
+import { useStrapi3 } from '../composables/useStrapi3'
+import { useStrapi4 } from '../composables/useStrapi4'
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin(async () => {

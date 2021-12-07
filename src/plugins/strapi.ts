d05ace9fedc88a9acdc6292497969009ac96635b
Benjamin Canac <canacb1@gmail.com>
2021-12-07 11:09:36 +0100

    fix(plugins): missing `defineNuxtPlugin`

--- src/plugins/strapi.ts
+++ src/plugins/strapi.ts
@@ -1,3 +1,5 @@
+import { defineNuxtPlugin } from '#app'
+
 export default defineNuxtPlugin(async () => {
   const version = useStrapiVersion()
   const { fetchUser } = version === 'v3' ? useStrapi3() : useStrapi4()

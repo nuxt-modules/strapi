6e25a4a884858590982a1efabc61a353d7e07dd3
Benjamin Canac <canacb1@gmail.com>
2021-12-20 11:53:35 +0100

    fix(useStrapiToken): implement caching

--- src/composables/useStrapiToken.ts
+++ src/composables/useStrapiToken.ts
@@ -1,3 +1,14 @@
-import { useCookie } from '#app'
+import { useCookie, useNuxtApp } from '#app'
 
-export const useStrapiToken = () => useCookie<string | null>('strapi_jwt')
+export const useStrapiToken = () => {
+  const nuxtApp = useNuxtApp()
+
+  nuxtApp._cookies = nuxtApp._cookies || {}
+  if (nuxtApp._cookies.strapi_jwt) {
+    return nuxtApp._cookies.strapi_jwt
+  }
+
+  const cookie = useCookie<string | null>('strapi_jwt')
+  nuxtApp._cookies.strapi_jwt = cookie
+  return cookie
+}

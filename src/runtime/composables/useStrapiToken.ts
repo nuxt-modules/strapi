6a7f642c1d1d35b64bc4d5584240bff74b52a4ef
Benjamin Canac <canacb1@gmail.com>
2022-01-12 18:21:47 +0100

    chore(useStrapiToken): put composable instance on top

--- src/runtime/composables/useStrapiToken.ts
+++ src/runtime/composables/useStrapiToken.ts
@@ -2,16 +2,14 @@ import { useCookie, useNuxtApp, useRuntimeConfig } from '#app'
 
 export const useStrapiToken = () => {
   const nuxtApp = useNuxtApp()
+  const config = useRuntimeConfig()
 
   nuxtApp._cookies = nuxtApp._cookies || {}
   if (nuxtApp._cookies.strapi_jwt) {
     return nuxtApp._cookies.strapi_jwt
   }
 
-  const config = useRuntimeConfig()
-  const opts = config.strapi.cookie
-
-  const cookie = useCookie<string | null>('strapi_jwt', opts)
+  const cookie = useCookie<string | null>('strapi_jwt', config.strapi.cookie)
   nuxtApp._cookies.strapi_jwt = cookie
   return cookie
 }

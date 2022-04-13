b7adc16da19c76c626392f7ac597fef54ea1a0ce
Benjamin Canac <canacb1@gmail.com>
2022-04-13 16:39:25 +0200

    fix(module): update runtime config override (#244)

--- src/runtime/composables/useStrapiGraphQL.ts
+++ src/runtime/composables/useStrapiGraphQL.ts
@@ -3,7 +3,7 @@ import { useStrapiClient } from './useStrapiClient'
 
 export const useStrapiGraphQL = () => {
   const client = useStrapiClient()
-  const config = useRuntimeConfig()
+  const config = useRuntimeConfig().public
 
   return <T> (query: string): Promise<T> => {
     return client('/graphql', { method: 'POST', body: { query }, baseURL: config.strapi.url })
--- src/runtime/composables/useStrapiUrl.ts
+++ src/runtime/composables/useStrapiUrl.ts
@@ -1,7 +1,7 @@
 import { useRuntimeConfig } from '#app'
 
 export const useStrapiUrl = (): string => {
-  const config = useRuntimeConfig()
+  const config = useRuntimeConfig().public
   const version = config.strapi.version
   return version === 'v3' ? config.strapi.url : `${config.strapi.url}${config.strapi.prefix}`
 }

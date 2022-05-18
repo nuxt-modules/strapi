49829fc48f10c73a4a7a0724bc0fa73511dc5353
Ted Klingenberg <ted@doseofted.com>
2022-05-17 23:59:45 -0400

    Added support for using imported graphql file and passing variables

--- src/runtime/composables/useStrapiGraphQL.ts
+++ src/runtime/composables/useStrapiGraphQL.ts
@@ -1,14 +1,19 @@
 import { useRuntimeConfig } from '#app'
+import { print } from 'graphql'
+import type { DocumentNode } from 'graphql'
 import { useStrapiClient } from './useStrapiClient'
 
 export const useStrapiGraphQL = () => {
   const client = useStrapiClient()
   const config = useRuntimeConfig().public
 
-  return <T> (query: string): Promise<T> => {
+  return <T> (query: string|DocumentNode, variables?: { [key: string]: unknown }): Promise<T> => {
     return client('/graphql', {
       method: 'POST',
-      body: { query },
+      body: {
+        query: typeof query === 'string' ? query : print(query),
+        variables
+      },
       headers: {
         accept: 'application/json'
       },

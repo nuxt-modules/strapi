f27d580764f26d36c0a71d465ba1ebcdd478d3a0
Benjamin Canac <canacb1@gmail.com>
2022-05-30 15:54:18 +0200

    up

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,7 +2,34 @@ import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
   extends: ['./node_modules/@docus/docs-theme'],
+  modules: ['@nuxthq/admin', '@docus/github', 'vue-plausible'],
   github: {
-    repo: 'nuxt-community/strapi-module'
+    owner: 'nuxt-community',
+    repo: 'strapi-module',
+    branch: 'dev'
+  },
+  plausible: {
+    domain: 'strapi.nuxtjs.org'
+  },
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            primary: {
+              100: '#FFFFFF',
+              200: '#F2EFFF',
+              300: '#D1C7FF',
+              400: '#AF9EFF',
+              500: '#8E75FF',
+              600: '#603DFF',
+              700: '#3205FF',
+              800: '#2500CC',
+              900: '#1B0094'
+            }
+          }
+        }
+      }
+    }
   }
 })

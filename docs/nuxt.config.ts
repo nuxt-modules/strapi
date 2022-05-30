b54d433a913bb7f31949e706c710e4627424e105
Benjamin Canac <canacb1@gmail.com>
2022-05-30 14:50:33 +0200

    docs: migrate to latest docus

---
+++ docs/nuxt.config.ts
@@ -0,0 +1,8 @@
+import { defineNuxtConfig } from 'nuxt'
+
+export default defineNuxtConfig({
+  extends: ['./node_modules/@docus/docs-theme'],
+  github: {
+    repo: 'nuxt-community/strapi-module'
+  }
+})

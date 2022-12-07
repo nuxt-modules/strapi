2b193d8755b072095cf62f49fe90bec0348503e0
Benjamin Canac <canacb1@gmail.com>
2022-12-07 11:28:18 +0100

    docs: fix app config lint

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,30 +1,30 @@
 export default defineAppConfig({
   docus: {
-    title: "Nuxt Strapi",
-    layout: "default",
-    url: "https://strapi.nuxtjs.org",
-    description: "@nuxtjs/strapi is a Nuxt module for first class integration with Strapi.",
+    title: 'Nuxt Strapi',
+    layout: 'default',
+    url: 'https://strapi.nuxtjs.org',
+    description: '@nuxtjs/strapi is a Nuxt module for first class integration with Strapi.',
     socials: {
-      github: "nuxt-modules/strapi",
-      twitter: "nuxt_js",
+      github: 'nuxt-modules/strapi',
+      twitter: 'nuxt_js'
     },
-    image: "/cover.jpg",
+    image: '/cover.jpg',
     header: {
-      logo: true,
+      logo: true
     },
     footer: {
       credits: {
-        icon: "IconDocus",
-        text: "Powered by Docus",
-        href: "https://docus.com",
+        icon: 'IconDocus',
+        text: 'Powered by Docus',
+        href: 'https://docus.com'
       },
       icons: [
         {
-          label: "NuxtJS",
-          href: "https://nuxtjs.org",
-          component: "IconNuxt",
-        },
-      ],
-    },
+          label: 'NuxtJS',
+          href: 'https://nuxtjs.org',
+          component: 'IconNuxt'
+        }
+      ]
+    }
   }
-});
+})

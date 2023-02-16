068490b083f52fc58fc991fe5c11d3631eae3dba
Christian Burkhart <1113278+jiblett1000@users.noreply.github.com>
2023-02-16 03:04:12 -0800

    useNuxtApp variable name consistency (#317)

--- src/runtime/composables/useStrapiToken.ts
+++ src/runtime/composables/useStrapiToken.ts
@@ -1,15 +1,15 @@
 import { useCookie, useNuxtApp, useRuntimeConfig } from '#app'
 
 export const useStrapiToken = () => {
-  const nuxtApp = useNuxtApp()
+  const nuxt = useNuxtApp()
   const config = useRuntimeConfig()
 
-  nuxtApp._cookies = nuxtApp._cookies || {}
-  if (nuxtApp._cookies[config.strapi.cookieName]) {
-    return nuxtApp._cookies[config.strapi.cookieName]
+  nuxt._cookies = nuxt._cookies || {}
+  if (nuxt._cookies[config.strapi.cookieName]) {
+    return nuxt._cookies[config.strapi.cookieName]
   }
 
   const cookie = useCookie<string | null>(config.strapi.cookieName, config.strapi.cookie)
-  nuxtApp._cookies[config.strapi.cookieName] = cookie
+  nuxt._cookies[config.strapi.cookieName] = cookie
   return cookie
 }

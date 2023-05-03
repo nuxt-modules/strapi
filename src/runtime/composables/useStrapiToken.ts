2ace2af519927edcab26e095f3828820688edcb1
Calvin Herizo <calvin.herizo@gmail.com>
2023-05-03 09:48:56 +0200

    fix nuxt cookies check condition and set cookie value

--- src/runtime/composables/useStrapiToken.ts
+++ src/runtime/composables/useStrapiToken.ts
@@ -5,11 +5,12 @@ export const useStrapiToken = () => {
   const config = process.server ? useRuntimeConfig() : useRuntimeConfig().public
 
   nuxt._cookies = nuxt._cookies || {}
-  if (nuxt._cookies[config.strapi.cookieName]) {
+  if (nuxt._cookies?.[config.strapi.cookieName]?.value) {
     return nuxt._cookies[config.strapi.cookieName]
   }
 
-  const cookie = useCookie<string | null>(config.strapi.cookieName, config.strapi.cookie)
+  const cookie = useCookie<string | null>(config.strapi.cookieName)
+  cookie.value = config.strapi.cookie
   nuxt._cookies[config.strapi.cookieName] = cookie
   return cookie
 }

eddcead24e116e0cb773c4941ad0f94ebc171a03
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-07-01 11:09:08 +0200

    docs: use docus (#476)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ docs/app/app.config.ts
@@ -0,0 +1,47 @@
+export default defineAppConfig({
+  header: {
+    title: 'Nuxt Strapi'
+  },
+  ui: {
+    colors: {
+      primary: 'indigo',
+      neutral: 'slate'
+    }
+  },
+  uiPro: {
+    footer: {
+      slots: {
+        root: 'border-t border-default'
+      }
+    },
+    contentNavigation: {
+      defaultVariants: {
+        variant: 'pill'
+      }
+    }
+  },
+  github: {
+    rootDir: 'docs'
+  },
+  socials: {
+    discord: 'https://discord.com/invite/ps2h6QT',
+    x: 'https://x.com/nuxt_js'
+  },
+  toc: {
+    title: 'Table of Contents',
+    bottom: {
+      title: 'Community',
+      links: [{
+        icon: 'i-simple-icons-nuxtdotjs',
+        label: 'Nuxt docs',
+        to: 'https://nuxt.com',
+        target: '_blank'
+      }, {
+        icon: 'i-simple-icons-strapi',
+        label: 'Strapi docs',
+        to: 'https://docs.strapi.io/dev-docs/intro',
+        target: '_blank'
+      }]
+    }
+  }
+})
--- docs/plugins/ui-components.ts
+++
@@ -1,6 +0,0 @@
-import { UBadge } from '#components'
-
-// Expose Nuxt UI components to Nuxt Content
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('UBadge', UBadge)
-})

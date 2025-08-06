3192d0dfa9835bec903d7ea26f85be4f4c389bd2
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-08-06 11:49:00 +0200

    docs(deps): upgrade to docus v4

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,11 +1,9 @@
 export default defineNuxtConfig({
+  extends: ['docus'],
   modules: ['@nuxtjs/plausible'],
   css: ['~/assets/css/main.css'],
   site: {
     name: 'Nuxt Strapi',
     url: 'https://strapi.nuxtjs.org'
-  },
-  future: {
-    compatibilityVersion: 4
   }
 })

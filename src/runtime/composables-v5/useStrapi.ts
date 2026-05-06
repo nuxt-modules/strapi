8b3e9771469759bb8c5eead8284d625c1ece1f79
Benjamin Canac <canacb1@gmail.com>
2026-05-06 19:13:28 +0200

    fix(v5): fix composable types and `delete` behavior
    - fix `'filter'` typo to `'filters'` in Omit types
    - change `delete` return type to `Promise<void>` (Strapi returns 204)
    - add `locale` param support to `delete`

--- src/runtime/composables-v5/useStrapi.ts
+++ src/runtime/composables-v5/useStrapi.ts
@@ -4,10 +4,10 @@ import { useStrapiClient } from '#imports'
 
 interface StrapiV5Client<T> {
   find<F = T>(contentType: string, params?: Strapi5RequestParams<F>, fetchOptions?: FetchOptions): Promise<Strapi5ResponseMany<F>>
-  findOne<F = T>(contentType: string, documentId: string, params?: Omit<Strapi5RequestParams<F>, 'filter'>, fetchOptions?: FetchOptions): Promise<Strapi5ResponseSingle<F>>
-  create<F = T>(contentType: string, data: Partial<F>, params?: Omit<Strapi5RequestParams<F>, 'filter'>): Promise<Strapi5ResponseSingle<F>>
-  update<F = T>(contentType: string, documentId: string | Partial<F>, data?: Partial<F>, params?: Omit<Strapi5RequestParams<F>, 'filter'>): Promise<Strapi5ResponseSingle<F>>
-  delete<F = T>(contentType: string, documentId?: string): Promise<Strapi5ResponseSingle<F>>
+  findOne<F = T>(contentType: string, documentId: string, params?: Omit<Strapi5RequestParams<F>, 'filters'>, fetchOptions?: FetchOptions): Promise<Strapi5ResponseSingle<F>>
+  create<F = T>(contentType: string, data: Partial<F>, params?: Omit<Strapi5RequestParams<F>, 'filters'>): Promise<Strapi5ResponseSingle<F>>
+  update<F = T>(contentType: string, documentId: string | Partial<F>, data?: Partial<F>, params?: Omit<Strapi5RequestParams<F>, 'filters'>): Promise<Strapi5ResponseSingle<F>>
+  delete(contentType: string, documentId?: string, params?: Pick<Strapi5RequestParams<never>, 'locale'>): Promise<void>
 }
 
 export const useStrapi = <T>(): StrapiV5Client<T> => {
@@ -29,10 +29,10 @@ export const useStrapi = <T>(): StrapiV5Client<T> => {
    *
    * @param  {string} contentType - Content type's name pluralized
    * @param  {string} documentId - ID of entry
-   * @param  {Omit<Strapi5RequestParams<T>, 'filter'>} [params] - Query parameters
+   * @param  {Omit<Strapi5RequestParams<T>, 'filters'>} [params] - Query parameters
    * @returns Promise<T>
    */
-  const findOne = <T>(contentType: string, documentId: string, params?: Omit<Strapi5RequestParams<T>, 'filter'>, fetchOptions?: FetchOptions): Promise<Strapi5ResponseSingle<T>> => {
+  const findOne = <T>(contentType: string, documentId: string, params?: Omit<Strapi5RequestParams<T>, 'filters'>, fetchOptions?: FetchOptions): Promise<Strapi5ResponseSingle<T>> => {
     const path = [contentType, documentId].filter(Boolean).join('/')
 
     return client(path, { method: 'GET', params, ...fetchOptions })
@@ -43,10 +43,10 @@ export const useStrapi = <T>(): StrapiV5Client<T> => {
    *
    * @param  {string} contentType - Content type's name pluralized
    * @param  {Record<string, any>} data - Form data
-   * @param  {Omit<Strapi5RequestParams<T>, 'filter'>} [params] - Query parameters
+   * @param  {Omit<Strapi5RequestParams<T>, 'filters'>} [params] - Query parameters
    * @returns Promise<T>
    */
-  const create = <T>(contentType: string, data: Partial<T>, params: Omit<Strapi5RequestParams<T>, 'filter'> = {}): Promise<Strapi5ResponseSingle<T>> => {
+  const create = <T>(contentType: string, data: Partial<T>, params: Omit<Strapi5RequestParams<T>, 'filters'> = {}): Promise<Strapi5ResponseSingle<T>> => {
     return client(`/${contentType}`, { method: 'POST', body: { data }, params })
   }
 
@@ -56,10 +56,10 @@ export const useStrapi = <T>(): StrapiV5Client<T> => {
    * @param  {string} contentType - Content type's name pluralized
    * @param  {string} documentId - ID of entry to be updated
    * @param  {Record<string, any>} data - Form data
-   * @param  {Omit<Strapi5RequestParams<T>, 'filter'>} [params] - Query parameters
+   * @param  {Omit<Strapi5RequestParams<T>, 'filters'>} [params] - Query parameters
    * @returns Promise<T>
    */
-  const update = <T>(contentType: string, documentId: string | Partial<T>, data?: Partial<T>, params: Omit<Strapi5RequestParams<T>, 'filter'> = {}): Promise<Strapi5ResponseSingle<T>> => {
+  const update = <T>(contentType: string, documentId: string | Partial<T>, data?: Partial<T>, params: Omit<Strapi5RequestParams<T>, 'filters'> = {}): Promise<Strapi5ResponseSingle<T>> => {
     if (typeof documentId === 'object') {
       data = documentId
       documentId = undefined
@@ -77,10 +77,10 @@ export const useStrapi = <T>(): StrapiV5Client<T> => {
    * @param  {string|number} id - ID of entry to be deleted
    * @returns Promise<T>
    */
-  const _delete = <T>(contentType: string, id?: string | number): Promise<T> => {
-    const path = [contentType, id].filter(Boolean).join('/')
+  const _delete = (contentType: string, documentId?: string, params?: Pick<Strapi5RequestParams<never>, 'locale'>): Promise<void> => {
+    const path = [contentType, documentId].filter(Boolean).join('/')
 
-    return client(path, { method: 'DELETE' })
+    return client(path, { method: 'DELETE', params })
   }
 
   return {
